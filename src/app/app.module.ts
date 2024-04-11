3fcae03cd190c62d74096d13abbf4ef07b1001c4
Joshua Debazar <debajoshua@gmail.com>
2024-04-11 23:03:23 +0000

    Angular files added

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
