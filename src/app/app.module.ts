320b3f83a703f66b6fc150a1ccb9875683f1676f
Joshua Debazar <debajoshua@gmail.com>
2024-04-11 23:41:24 +0000

    State Files Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { booksReducer } from './book-list/books.reducer';
+import { collectionReducer } from './book-list/state/collection.reducer';
+import {HttpClientModule} from '@angular/common/http';
+import { BookListComponent } from './book-list/book-list.component'
 
 @NgModule({
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
-  declarations: [AppComponent],
+  imports: [
+          BrowserModule, 
+          StoreModule.forRoot({books : booksReducer, collection : collectionReducer}),
+          HttpClientModule],
+  declarations: [AppComponent, BookListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
