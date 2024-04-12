2941c1b048715ebcab7cfd6afb76350f7353ac38
Joshua Debazar <debajoshua@gmail.com>
2024-04-12 00:04:23 +0000

    styling added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GoogleBooksService } from './book-list/books.service';
+import { selectBookCollection,selectBooks } from './book-list/state/books.selectors';
+import { BooksActions,BooksApiActions } from './book-list/state/books.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  onAdd(bookId:string){
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId:string){
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
+
+  constructor(private store : Store, private booksService: GoogleBooksService){}
+
+  ngOnInit(){
+    this.booksService.getBooks()
+    .subscribe((books) => 
+    this.store.dispatch(BooksApiActions.retrievedBookList({books})));
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { booksReducer } from './book-list/books.reducer';
 import { collectionReducer } from './book-list/state/collection.reducer';
-import {HttpClientModule} from '@angular/common/http';
-import { BookListComponent } from './book-list/book-list.component'
+import { HttpClientModule } from '@angular/common/http';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component'
 
 @NgModule({
   imports: [
           BrowserModule, 
           StoreModule.forRoot({books : booksReducer, collection : collectionReducer}),
-          HttpClientModule],
-  declarations: [AppComponent, BookListComponent],
+          HttpClientModule,
+          CommonModule],
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
